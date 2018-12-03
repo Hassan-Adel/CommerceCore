fe340091988c3a5fac5945b4c04cc74097281b91
hassan adel <hasanadel555@hotmail.com>
2018-12-03 03:29:39 +0200

    Add register component

---
+++ CommerceCore/ClientApp/src/app/account/register.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthenticationService } from './../services/authentication.service'
+import { CustomValidatorService } from './../shared/custom-validators';
+
+
+@Component({
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+  form : any;
+  constructor(private authApi : AuthenticationService, private FB : FormBuilder, customValidaors : CustomValidatorService) {
+      this.form = FB.group({
+        email:['', [Validators.required, customValidaors.emailValid()]],
+        password:['', Validators.required],
+        confirmPassword:['', Validators.required]
+      }
+      ,{ validator: customValidaors.matchingFields('password', 'confirmPassword')}
+      
+      );
+   }
+
+   register(){
+      this.authApi.register(this.form.value);
+    }
+}
