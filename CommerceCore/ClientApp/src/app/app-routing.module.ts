f5149d11d8ed596d17b95412d3941ef62fce8aa1
hassan adel <hasanadel555@hotmail.com>
2018-12-07 23:28:26 +0200

    Adding login routes

--- CommerceCore/ClientApp/src/app/app-routing.module.ts
+++ CommerceCore/ClientApp/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { RegisterComponent } from './account/register.component';
+import { LoginComponent } from './account/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
   { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },//anytime user navigates to root take them to books, use pathMach whenever using redirecting
   { path: '**', redirectTo: '', pathMatch: 'full' }// '**' (wild card) when a user navigates to route that isn't defined , ie: 404
 ];
