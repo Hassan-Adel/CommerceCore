f26d0bc4910448315a094683ea051a9f98012a96
hassan adel <hasanadel555@hotmail.com>
2018-12-17 00:20:06 +0200

    Adding RegistrationViewModelValidatorusing FluentValidator

---
+++ CommerceCore/ViewModels/Validations/RegistrationViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using CommerceCore.ViewModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceCore.Models.Validations
+{
+    public class RegistrationViewModelValidator : AbstractValidator<RegistrationViewModel>
+    {
+        public RegistrationViewModelValidator()
+        {
+            RuleFor(vm => vm.Email).NotEmpty().WithMessage("Email cannot be empty");
+            RuleFor(vm => vm.Password).NotEmpty().WithMessage("Password cannot be empty");
+            RuleFor(vm => vm.FirstName).NotEmpty().WithMessage("FirstName cannot be empty");
+            RuleFor(vm => vm.LastName).NotEmpty().WithMessage("LastName cannot be empty");
+            RuleFor(vm => vm.Location).NotEmpty().WithMessage("Location cannot be empty");
+        }
+    }
+}
