d69b2c59dd288fe34fc02ed379858705a0cd8bec
hassan adel <hasanadel555@hotmail.com>
2018-12-17 15:28:10 +0200

    Add DashboardConroller

---
+++ CommerceCore/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using CommerceCore.DAL;
+using CommerceCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CommerceCore.Controllers
+{
+    [Authorize(Policy = "ApiUser")]
+    [Route("api/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        private readonly ClaimsPrincipal _caller;
+        private readonly UserDBContext _appDbContext;
+
+        public DashboardController(UserManager<AppUser> userManager, UserDBContext appDbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _caller = httpContextAccessor.HttpContext.User;
+            _appDbContext = appDbContext;
+        }
+    }
+}
