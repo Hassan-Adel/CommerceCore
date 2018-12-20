3ac17e756ea0b2af9bd4a05bd4f5bfdfa82dc246
hassan adel <hasanadel555@hotmail.com>
2018-12-20 19:29:50 +0200

    Add dashboard controller

--- CommerceCore/Controllers/DashboardController.cs
+++ CommerceCore/Controllers/DashboardController.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace CommerceCore.Controllers
 {
@@ -19,6 +22,27 @@ namespace CommerceCore.Controllers
         {
             _caller = httpContextAccessor.HttpContext.User;
             _appDbContext = appDbContext;
+        }// GET api/dashboard/home
+
+        [HttpGet]
+        public async Task<IActionResult> Home()
+        {
+            // retrieve the user info
+            //HttpContext.User
+            var userId = _caller.Claims.Single(c => c.Type == "id");
+            var customer = await _appDbContext.Customers.Include(c => c.Identity).SingleAsync(c => c.Identity.Id == userId.Value);
+
+            return new OkObjectResult(new
+            {
+                Message = "This is secure API and user data!",
+                customer.Identity.FirstName,
+                customer.Identity.LastName,
+                customer.Identity.PictureUrl,
+                customer.Identity.FacebookId,
+                customer.Location,
+                customer.Locale,
+                customer.Gender
+            });
         }
     }
 }
