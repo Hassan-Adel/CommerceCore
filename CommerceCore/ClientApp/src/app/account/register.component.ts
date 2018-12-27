fbd7c08464f76c89a4acf1ac476226634b649c49
hassan adel <hasanadel555@hotmail.com>
2018-12-27 18:12:49 +0200

    Updating register component

--- CommerceCore/ClientApp/src/app/account/register.component.ts
+++ CommerceCore/ClientApp/src/app/account/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthenticationService } from './../services/authentication.service'
 import { CustomValidatorService } from './../shared/custom-validators';
 
@@ -8,11 +9,19 @@ import { CustomValidatorService } from './../shared/custom-validators';
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-  form : any;
-  constructor(private authApi : AuthenticationService, private FB : FormBuilder, customValidaors : CustomValidatorService) {
+
+  form: any;
+  errors: string;
+  isRequesting: boolean;
+  submitted: boolean = false;
+
+  constructor(private authApi: AuthenticationService, private FB: FormBuilder, customValidaors: CustomValidatorService, private router: Router) {
       this.form = FB.group({
         email:['', [Validators.required, customValidaors.emailValid()]],
-        password:['', Validators.required],
+        password: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        location: ['', Validators.required],
         confirmPassword:['', Validators.required]
       }
       ,{ validator: customValidaors.matchingFields('password', 'confirmPassword')}
@@ -20,7 +29,20 @@ export class RegisterComponent {
       );
    }
 
-   register(){
-      this.authApi.register(this.form.value);
+  register() {
+
+    this.submitted = true;
+    this.isRequesting = true;
+    this.errors = '';
+
+     this.authApi.register(this.form.value)
+       .finally(() => this.isRequesting = false)
+       .subscribe(
+         result => {
+           if (result) {
+             this.router.navigate(['/login'], { queryParams: { brandNew: true, email: this.form.email } });
+           }
+         },
+         errors => this.errors = errors);
     }
 }
